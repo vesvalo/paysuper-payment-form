a708093b54b4a988bc3f56560f4e7d0a55dd773d
Andrey Solodovnikov <a.solodovnikov@office.ngs.ru>
2019-01-14 15:46:55 +0300

    PAY-72 more coverage

---
+++ tests/unit/BaseButton.spec.js
@@ -0,0 +1,13 @@
+import { shallowMount } from '@vue/test-utils';
+import BaseButton from '@/components/BaseButton.vue';
+
+describe('BaseButton.vue', () => {
+  it('should render content', () => {
+    const wrapper = shallowMount(BaseButton, {
+      slots: {
+        default: 'check',
+      },
+    });
+    expect(wrapper.text()).toEqual('check');
+  });
+});
