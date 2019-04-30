a811fe7cd07d408a757235a51fd1f8530be30f7d
Pavel Kokovin <pavel.kokovin@protocol.one>
2019-04-30 16:47:45 +0700

    Add UiButton

--- src/Sandbox.vue
+++ src/Sandbox.vue
@@ -1,9 +1,50 @@
+<script>
+import UiButton from './components/UiButton.vue';
+
+export default {
+  components: { UiButton },
+  data() {
+    return {
+      hasClick: false,
+    };
+  },
+  methods: {
+    clickHandler() {
+      this.hasClick = !this.hasClick;
+    },
+  },
+};
+</script>
+
 <template>
-  <div>
+<div class="sandbox">
+  <h1>
     Sandbox is here
+  </h1>
+
+  <div class="ui-kit">
+    <UiButton @click="clickHandler" :disabled="true">
+      <template v-if="hasClick" slot="before">Before</template>
+      Text for button
+      <template v-if="!hasClick" slot="after">After</template>
+    </UiButton>
   </div>
+</div>
 </template>
 
 <style lang="scss">
 @import "@/assets/styles/reset.scss";
+
+.sandbox {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+.ui-kit {
+  width: 100vw;
+  flex-grow: 1;
+  background-color: #424c66;
+  padding: 20px 50px;
+}
 </style>
---
+++ src/components/UiButton.vue
@@ -0,0 +1,104 @@
+<template>
+<button
+  :class="['ui-button', { '_disabled': disabled } ]"
+  @click="onClick"
+>
+  <span class="before">
+    <slot name="before" />
+  </span>
+
+  <slot />
+
+  <span class="after">
+    <slot name="after" />
+  </span>
+</button>
+</template>
+
+<script>
+export default {
+  props: {
+    disabled: {
+      default: false,
+      type: Boolean,
+    },
+  },
+  methods: {
+    onClick(event) {
+      // If button has disabled we shoudn't send events
+      if (this.disabled) {
+        return;
+      }
+
+      this.$emit('click', event);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css?family=Comfortaa:300,400,700|Quicksand&subset=cyrillic,cyrillic-ext');
+
+$after-margin: 12px;
+$before-margin: 12px;
+$after-text-color: #fff;
+$before-text-color: #fff;
+$box-color: #5b88de;
+$font-family: 'Quicksand', 'Comfortaa', sans-serif;
+$font-size: 20px;
+$font-weight: 700;
+$height: 70px;
+$padding: 0 24px;
+$text-color: #fff;
+$align: center;
+$border-width: 0;
+$border-color: transparent;
+$hover-box-color: lighten($box-color, 3%);
+$active-box-color: darken($box-color, 3%);
+$disabled-opacity: 0.7;
+$border-radius: 12px;
+$transition: background-color 0.2s ease-out, color 0.2s ease-out;
+
+.ui-button {
+  cursor: pointer;
+  outline-width: 0;
+  position: relative;
+  display: inline-flex;
+  background-color: $box-color;
+  font-family: $font-family;
+  font-size: $font-size;
+  font-weight: $font-weight;
+  height: $height;
+  padding: $padding;
+  color: $text-color;
+  align-items: center;
+  justify-content: $align;
+  border-width: $border-width;
+  border-color: $border-color;
+  border-radius: $border-radius;
+  transition: $transition;
+
+  &:hover {
+    background-color: $hover-box-color;
+  }
+
+  &:active {
+    background-color: $active-box-color;
+  }
+
+  &._disabled {
+    pointer-events: none;
+    opacity: $disabled-opacity;
+  }
+}
+.before {
+  display: inline-block;
+  color: $before-text-color;
+  margin-right: $before-margin;
+}
+.after {
+  display: inline-block;
+  color: $after-text-color;
+  margin-left: $after-margin;
+}
+</style>
