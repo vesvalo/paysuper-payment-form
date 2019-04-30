27b8ab62bbe77d99152f978aa6acfa68ec412087
Pavel Kokovin <pavel.kokovin@protocol.one>
2019-04-30 14:41:59 +0700

    Fix lint

--- src/Sandbox.vue
+++ src/Sandbox.vue
@@ -1,16 +1,21 @@
 <script>
 import UiButton from './components/UiButton.vue';
+import UiCheckbox from './components/UiCheckbox.vue';
 
 export default {
-  components: { UiButton },
+  components: { UiButton, UiCheckbox },
   data() {
     return {
       hasClick: false,
+      check: false,
     };
   },
   methods: {
     clickHandler() {
       this.hasClick = !this.hasClick;
+    },
+    checkHandler(value) {
+      this.check = value;
     },
   },
 };
@@ -23,11 +28,22 @@ export default {
   </h1>
 
   <div class="ui-kit">
-    <UiButton @click="clickHandler" :disabled="true">
+    <UiButton
+      class="ui-item"
+      @click="clickHandler"
+      :disabled="true"
+    >
       <template v-if="hasClick" slot="before">Before</template>
       Text for button
       <template v-if="!hasClick" slot="after">After</template>
     </UiButton>
+    <UiCheckbox
+      v-model="check"
+      class="ui-item"
+      @input="checkHandler"
+    >
+      Label for checkbox
+    </UiCheckbox>
   </div>
 </div>
 </template>
@@ -46,5 +62,9 @@ export default {
   flex-grow: 1;
   background-color: #424c66;
   padding: 20px 50px;
+}
+.ui-item {
+  display: block;
+  margin-bottom: 20px;
 }
 </style>
