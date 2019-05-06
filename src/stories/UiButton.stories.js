64697e7a879a039a49731c7e55c69d6603ed8aa4
Pavel Kokovin <pavel.kokovin@protocol.one>
2019-05-06 11:00:29 +0700

    Add component to storybook

---
+++ src/stories/UiButton.stories.js
@@ -0,0 +1,33 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { storiesOf } from '@storybook/vue';
+import { action } from '@storybook/addon-actions';
+
+import UiButton from '../components/UiButton.vue';
+
+storiesOf('UiButton', module)
+  .add('simple', () => ({
+    components: { UiButton },
+    render() {
+      return <UiButton onClick={this.action}>Some text</UiButton>;
+    },
+    methods: { action: action('clicked') },
+  }))
+  .add('with before and after content', () => ({
+    components: { UiButton },
+    render() {
+      return (
+        <UiButton onClick={this.action}>
+          <template slot="before">(before content)</template>
+          Some text
+          <template slot="after">(after content)</template>
+        </UiButton>
+      );
+    },
+    methods: { action: action('clicked') },
+  }))
+  .add('disabled', () => ({
+    components: { UiButton },
+    render() {
+      return <UiButton disabled={true}>Some text</UiButton>;
+    },
+  }));
