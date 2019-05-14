4744cc463485e961126bfd9d1ba2edd84c69289a
Pavel Kokovin <pavel.kokovin@protocol.one>
2019-05-14 17:06:47 +0700

    Add icons

---
+++ src/components/IconCard.vue
@@ -0,0 +1,6 @@
+<template>
+<svg width="18" height="14" viewBox="0 0 18 14" fill="none">
+  <rect x="0.5" y="0.5" width="17" height="13" rx="2.5" stroke="#06EAA7"/>
+  <rect y="4" width="18" height="3" fill="#06EAA7"/>
+</svg>
+</template>;
