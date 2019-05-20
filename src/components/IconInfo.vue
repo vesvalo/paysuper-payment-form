c0317308e81096778e6949defc698771edffcdb9
Pavel Kokovin <pavel.kokovin@protocol.one>
2019-05-20 15:11:46 +0700

    Fix quotes for icons

--- src/components/IconInfo.vue
+++ src/components/IconInfo.vue
@@ -1,14 +1,14 @@
 <template>
-<svg width='12' height='12' viewBox='0 0 12 12' fill='none'>
+<svg width="12" height="12" viewBox="0 0 12 12" fill="none">
   <path
-    fill-rule='evenodd'
-    clip-rule='evenodd'
-    d='M12 6C12 9.31371 9.31371 12 6 12C2.68629 12 0 9.31371 0 6C0 2.68629 2.68629 0 6 0C9.31371
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    d="M12 6C12 9.31371 9.31371 12 6 12C2.68629 12 0 9.31371 0 6C0 2.68629 2.68629 0 6 0C9.31371
       0 12 2.68629 12 6ZM5 6C5 5.44772 5.44772 5 6 5C6.55228 5 7 5.44772 7 6V9C7 9.55229 6.55228
       10 6 10C5.44772 10 5 9.55229 5 9V6ZM6 2C5.44772 2 5 2.44772 5 3C5 3.55228 5.44772 4 6
-      4C6.55228 4 7 3.55228 7 3C7 2.44772 6.55228 2 6 2Z'
-    fill='white'
-    fill-opacity='0.3'
+      4C6.55228 4 7 3.55228 7 3C7 2.44772 6.55228 2 6 2Z"
+    fill="white"
+    fill-opacity="0.3"
   />
 </svg>
-</template>;
+</template>
