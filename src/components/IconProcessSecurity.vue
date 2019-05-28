ecf7f3d416201bb073b460ddbb31898db4338480
Andrey Solodovnikov <a.solodovnikov@office.ngs.ru>
2019-05-28 10:18:00 +0300

    action processing screen

---
+++ src/components/IconProcessSecurity.vue
@@ -0,0 +1,16 @@
+<template>
+<svg width="139" height="137" viewBox="0 0 139 137" fill="none" xmlns="http://www.w3.org/2000/svg">
+<!-- eslint-disable -->
+<path d="M90.8337 99.5036C90.8337 99.5036 126.349 81.9985 126.349 59.9437C126.349 37.889 126.349 34.1267 126.349 34.1267L90.8337 14.0146L57.1168 34.1267C57.1168 34.1267 57.1168 35.8697 57.1168 43.4452" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M45.9051 28.0292L83.7445 5.56207" stroke="#5B88DE" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+<circle cx="90.8503" cy="1.5" r="1.5" fill="#5B88DE"/>
+<path d="M137 60.2628C137 87.8672 110.478 103.4 97.6031 109.314" stroke="#5B88DE" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+<circle cx="90.8503" cy="112.215" r="1.5" fill="#5B88DE"/>
+<circle cx="90.7518" cy="46.2481" r="8.31022" stroke="white" stroke-width="3"/>
+<path d="M90.7518 54.6569L90.7518 75.6788L96.3577 75.6788" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M90.7518 65.8686H96.3577" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+<circle cx="44" cy="100" r="37" fill="#333B50"/>
+<circle cx="39" cy="90" r="37" stroke="white" stroke-width="3"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M42.2427 73.1716C43.8047 71.6095 46.3374 71.6095 47.8994 73.1716L61.5098 86.7819C62.3932 87.6653 62.7771 88.8591 62.6613 90.0123C62.7771 91.1655 62.3932 92.3593 61.5098 93.2427L47.8994 106.853C46.3374 108.415 43.8047 108.415 42.2427 106.853C40.6805 105.291 40.6805 102.758 42.2427 101.196L50.0956 93.3431H19C16.7909 93.3431 15 91.5523 15 89.3431C15 87.134 16.7909 85.3431 19 85.3431H48.7573L42.2427 78.8284C40.6805 77.2663 40.6805 74.7337 42.2427 73.1716Z" fill="#05EAA7"/>
+</svg>
+</template>
