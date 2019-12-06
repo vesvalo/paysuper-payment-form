ec1909cbb93027e50a167f31adfc47986b6954a5
Andrey Solodovnikov <a.solodovnikov@office.ngs.ru>
2019-12-06 18:45:32 +0300

    global withCredentials=true 2

--- src/plugins/extendAxios.js
+++ src/plugins/extendAxios.js
@@ -1,12 +1,8 @@
 import axios from 'axios';
 
-export const buildPurpose = process.env.VUE_APP_BUILD_PURPOSE;
-
-export default function extendAxios() {
-  const createSetAuthInterceptor = () => config => ({
-    ...config,
-    withCredentials: true,
-  });
-  const setAuthCb = createSetAuthInterceptor();
-  axios.interceptors.request.use(setAuthCb);
-}
+const createSetAuthInterceptor = () => config => ({
+  ...config,
+  withCredentials: true,
+});
+const setAuthCb = createSetAuthInterceptor();
+axios.interceptors.request.use(setAuthCb);
