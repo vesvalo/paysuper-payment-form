68a86595aa0bac3d9345da0901e98a3c7cc81a7c
Andrey Solodovnikov <a.solodovnikov@office.ngs.ru>
2019-12-10 17:05:43 +0300

    190560 fixing some analytic events

--- src/store/PaymentFormStore.js
+++ src/store/PaymentFormStore.js
@@ -364,6 +364,7 @@ export default {
     },
 
     async removeCard({ commit, state, rootState }, id) {
+      gtagEvent('submitSavedCardRemove');
       try {
         await axios.delete(
           `${rootState.apiUrl}/api/v1/saved_card`,
@@ -377,7 +378,6 @@ export default {
       } catch (error) {
         console.error(error);
       }
-      gtagEvent('removeRememberedCard', { event_category: 'userAction' });
     },
 
     async checkPaymentAccount({
